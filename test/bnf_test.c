506237c37726c5961b7bd4144616d7c55ce581b8
Philippe Caron <philippe-caron@hotmail.com>
2019-06-23 01:02:28 +0300

    Progress

--- test/bnf_test.c
+++ test/bnf_test.c
@@ -1,6 +1,57 @@
 #include <bnf_parser.h>
+#include <terminal.h>
+
+#define BRANCH    "│"
+#define NODE      "├"
+#define LAST_NODE "└"
+
+void printnode(Node *n, int lvl) {
+  for (int i = 0; i < lvl - 1; i++) {
+    printf(BRANCH);
+  }
+  switch (n->type) {
+  case NODE_ROOT:
+    printf(TEXT_BLUE""FONT_BOLD"[#] %s\n"FONT_RESET, n->name);
+    for (int i = 0; i < n->num; i++) {
+      printnode(((Node**)n->content)[i], lvl + 1);
+      printf("what?\n");
+    }
+    break;
+  case NODE_LEAF:
+    printf("%s\n", (char*)n->content);
+    break;
+  case NODE_LIST:
+    printf("::\n");
+    for (int i = 0; i < n->num; i++) {
+      printnode(((Node**)n->content)[i], lvl + 1);
+    }
+    break;
+  case NODE_ONE_OF:
+    printf(TEXT_BLUE"%s\n"FONT_RESET, n->name);
+    for (int i = 0; i < n->num; i++) {
+      printnode(((Node**)n->content)[i], lvl + 1);
+    }
+    break;
+  case NODE_ONE_OR_NONE:
+    printf("[+]");
+    for (int i = 0; i < n->num; i++) {
+      printnode(((Node**)n->content)[i], lvl + 1);
+    }
+    break;
+  case NODE_MANY_OR_NONE:
+    printf("[*]");
+    for (int i = 0; i < n->num; i++) {
+      printnode(((Node**)n->content)[i], lvl + 1);
+    }
+    break;
+  case NODE_UNKNOWN:
+    printf("UNKNOWN\n");
+    break;
+  }
+}
 
 int main() {
-  parsefile("parsing/bnf/test.bnf");
+  Node *n = parsefile("parsing/bnf/test.bnf");
+  printnode(n, 0);
   return 0;
 }
