d27f9b2b5cc49212c43a306fd7fc6cda4ccbd9c8
Philippe Caron <philippe-caron@hotmail.com>
2020-12-14 01:45:25 -0500

    Testing again # Describe briefly the new features introduced in this build, the bug fixes, etc. # Build author name:  Philippe Caron # Build author email: philippe-caron@hotmail.com # Build verison:      0.0.0.4 # Build time:         2020-12-14 01:45:19

--- cisor/inc/version.h
+++ cisor/inc/version.h
@@ -1,6 +1,6 @@
 #define VERSION_MAJOR    0
 #define VERSION_MINOR    0
 #define VERSION_REVISION 0
-#define VERSION_BUILD    2
+#define VERSION_BUILD    3
 
-#define BUILD_DATE       "2020-12-14"
+#define BUILD_DATE       "%F"
